d2ad65b3c2b84a722e73fe992b1b8d1f0cb76189
Jonas B <jonas.bellemans@hotmail.com>
2020-10-13 13:37:56 +0200

    Updates

---
+++ precache-manifest.a592bc030ea8fcdfb51caf3fe43b9893.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac284b86c0eb453711138c2f278f671d",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "58658d0f6d59718703cc",
+    "url": "/portfolio-react/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "639087665d8590a2e964",
+    "url": "/portfolio-react/static/css/4.ee2a0142.chunk.css"
+  },
+  {
+    "revision": "eb376f45cc6603a69ddd",
+    "url": "/portfolio-react/static/css/main.a0be3aaa.chunk.css"
+  },
+  {
+    "revision": "d03c9cf0c8e2460bd876",
+    "url": "/portfolio-react/static/js/2.0f327097.chunk.js"
+  },
+  {
+    "revision": "58658d0f6d59718703cc",
+    "url": "/portfolio-react/static/js/3.735efde7.chunk.js"
+  },
+  {
+    "revision": "639087665d8590a2e964",
+    "url": "/portfolio-react/static/js/4.f5f49485.chunk.js"
+  },
+  {
+    "revision": "eb376f45cc6603a69ddd",
+    "url": "/portfolio-react/static/js/main.6de4a9ec.chunk.js"
+  },
+  {
+    "revision": "fa55b2971b1a838cdcc2",
+    "url": "/portfolio-react/static/js/runtime-main.e7d753e5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio-react/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio-react/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/portfolio-react/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio-react/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio-react/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio-react/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio-react/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/portfolio-react/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/portfolio-react/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio-react/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "83a05b10639d238bd8c5bbab279ab911",
+    "url": "/portfolio-react/static/media/harvardLogo.83a05b10.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/portfolio-react/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio-react/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio-react/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/portfolio-react/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/portfolio-react/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/portfolio-react/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio-react/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/portfolio-react/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/portfolio-react/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio-react/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/portfolio-react/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/portfolio-react/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "7f0feeedda08cf7d68593454bc2e82b5",
+    "url": "/portfolio-react/static/media/talksCardBackAlt.7f0feeed.svg"
+  }
+]);
