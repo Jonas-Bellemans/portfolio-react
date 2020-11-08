63a1608a09b0dda11aa12e1b54afca943eac37d2
Jonas B <jonas.bellemans@hotmail.com>
2020-11-08 16:37:35 +0100

    Added "avatar" greeting image

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -45,7 +45,7 @@ export default function Greeting() {
           <div className="greeting-image-div">
             <img
               alt="JB sitting on table"
-              src={require("../../assets/images/manOnTable.svg")}
+              src={require("../../assets/images/avatar-enthousiant-hi.svg")}
             ></img>
           </div>
         </div>
